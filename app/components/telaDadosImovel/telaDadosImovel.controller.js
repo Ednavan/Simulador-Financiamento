14af2839c5287b381fdeb0954425795eadfa291c
Ednavan <ednavanfisico@gmail.com>
2021-10-29 17:14:40 -0300

    fazendo um testes do controller imovel realizando um post para o db.json

--- app/components/telaDadosImovel/telaDadosImovel.controller.js
+++ app/components/telaDadosImovel/telaDadosImovel.controller.js
@@ -1,5 +1,28 @@
-sincronizando.controller('ctrlTelaDadosImovel', function($scope, $location){
+sincronizando.controller('ctrlTelaDadosImovel', function($scope, $location, $http){
     $scope.tituloHeadPageImovel = "Dados do Imóvel"
+
+
+    $scope.data = {}
+
+    $scope.envioDados= function (){
+        console.log('chegou')
+        $http({
+            url: 'http://localhost:3000/bancoDeListaDados/',
+            method: 'POST',
+            data: $scope.data
+        }).then(function(){
+            console.log('sucess')
+        })
+       
+    }
+    $http.get('http://localhost:3000/bancoDeListaDados/').
+    then(function(res){
+        (console.log(res))
+        $scope.bancoDeListaDados = res.data
+    })
+
+
+
     $scope.irProximaPage = function(){
         $location.path('/TelaDadosProponente')
     }

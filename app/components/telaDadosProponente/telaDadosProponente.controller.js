4d3505b487794fad60f6d2de8b35b33faf65aa2f
Ednavan <ednavanfisico@gmail.com>
2021-10-30 15:45:21 -0300

    análise de testes para salvar informações na memória da tela DadosProponente

--- app/components/telaDadosProponente/telaDadosProponente.controller.js
+++ app/components/telaDadosProponente/telaDadosProponente.controller.js
@@ -1,7 +1,24 @@
 sincronizando.controller('ctrlTelaDadosProponent', function($scope,$location){
     $scope.tituloHeadPage = "Dados Proponente"
 
-    $scope.irProximaPage = function(){
-        $location.path('/TelaDadosImovel')
+    $scope.irProximaPage = function(nome, profi, cpf, email, nascimento, cep, celular){
+    
+     
+        var recebendoDados = {
+            
+            dadosDoNome:  $scope.infoNomeCompleto,
+            dadosDaProfissao: $scope.infoProfissao,
+            dadosDoCpf: $scope.infoCPF,
+            dadosDoEmail: $scope.infoEmail,
+            dadosDoNascimento: $scope.infoDataNascimento,
+            dadosDoCep: $scope.infoCEP,
+            dadosDoCelular: $scope.infoCelular
+
+           
+            
+        }
+        $location.search(recebendoDados)
+        console.log(recebendoDados)
+        // $location.path('/TelaDadosImovel')
     }
 })

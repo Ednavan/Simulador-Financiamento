fcdee0d25cd33d0776207522474f5a82671c8f84
Ednavan Lima Silva <elimsilv@everis.com>
2021-11-23 10:10:05 -0300

    cricação dos botões de visualizar e excluir

--- app/components/telaAusenciaDeHistoricoPropostas/telaAusenciaDeHistorico.controller.js
+++ app/components/telaAusenciaDeHistoricoPropostas/telaAusenciaDeHistorico.controller.js
@@ -1,3 +1,8 @@
 sincronizando.controller('ctrlAusenciaHistoricoPropostas',function($scope,$location, $http){
     $scope.headTituloInicial = "Histórico de propostas"
+
+    $scope.irParaHome = function(){
+
+        $location.path('/telaInicio')
+    }
 })

6c13cf72b985b3e048255c5c6293863ace441e79
Ednavan Lima Silva <elimsilv@everis.com>
2021-11-22 21:58:11 -0300

    ao clicar no botão usência de de dados no histórico, se caso, não conter dados no db.json, mas caso tenha dados já inseridos, abrirá a tela de Lista de Propostas

--- app/components/tela-Inicial/telaInicial.controller.js
+++ app/components/tela-Inicial/telaInicial.controller.js
@@ -1,15 +1,21 @@
-sincronizando.controller('ctrlTelaInicial', function($scope, $location){
+sincronizando.controller('ctrlTelaInicial', function($scope, $location, $http){
     $scope.headTituloInicial = "Simulador de Financiamentos"
     $scope.irProximaPage = function(){
         $location.path('/TelaDadosProponente')
     }
 
     $scope.historicoPropostas = function(){
+      console.log('Checargem de dados no banco de dados',$http.get('http://localhost:3000/bancoDeListaDados/').
+      then(function (res) {
+          if(res.data == 0){
+              $location.path('/ausenciaHistoricoProposta')
+              
+            }else{
+              $location.path('/listaPropostas')
+          }
 
+      }))
 
-        
-        $location.path('/listaPropostas')
-    }
+      };
 
-    
 })

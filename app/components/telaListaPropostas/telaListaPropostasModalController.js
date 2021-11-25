4578e96fa03a1df4564dd8414293509b01c88f7c
Ednavan Lima Silva <elimsilv@everis.com>
2021-11-25 10:06:30 -0300

    criacao da telaListaPropostasModalController, responsavel por realizar busca dos dados

---
+++ app/components/telaListaPropostas/telaListaPropostasModalController.js
@@ -0,0 +1,4 @@
+sincronizando.controller("ctrlModalbtnHistorico", function ($scope,sincronizacaoServiceListagem) {
+    // objetividade de realizar uma busca de dados
+    $scope.atualizaListagem = sincronizacaoServiceListagem;
+});
